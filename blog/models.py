7b3c73d618cad05f0fd84a4681a22d0f803aa625
spicy-sauce911 <Andrew.novoselov99@mail.ru>
2023-02-01 10:56:55 +0300

    add home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	post_title = models.CharField(max_length=300)
+	post_time = models.DateTimeField(auto_now=True)
+	post_image = models.ImageField(upload_to='event_images/')
+	post_text = models.TextField(max_length=300)
